5d8038f3670c2c1167f1cf80a41e391acd50cb54
Diogo RBG <diogorbg@gmail.com>
2025-10-25 21:32:30 -0300

    scroll resolvido

--- godotQuebraCabeca/quebraCabeca.service.worker.js
+++ godotQuebraCabeca/quebraCabeca.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761420443|7243956703';
+const CACHE_VERSION = '1761438674|15759086486';
 /** @type {string} */
 const CACHE_PREFIX = 'Quebra Cabeça-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
