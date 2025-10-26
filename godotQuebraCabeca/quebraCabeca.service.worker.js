f93d36b496569abbb4d47ab6cc68860cd9d602d9
Diogo RBG <diogorbg@gmail.com>
2025-10-26 16:37:43 -0300

    add colmeia

--- godotQuebraCabeca/quebraCabeca.service.worker.js
+++ godotQuebraCabeca/quebraCabeca.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761438674|15759086486';
+const CACHE_VERSION = '1761507392|84479115764';
 /** @type {string} */
 const CACHE_PREFIX = 'Quebra Cabeça-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
