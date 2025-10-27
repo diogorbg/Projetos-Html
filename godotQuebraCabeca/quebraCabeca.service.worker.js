29c3c3be24d2e81120260272cbe2fc75b55f637e
Diogo RBG <diogorbg@gmail.com>
2025-10-27 09:00:28 -0300

    add mosaico completo

--- godotQuebraCabeca/quebraCabeca.service.worker.js
+++ godotQuebraCabeca/quebraCabeca.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761530133|13153879343';
+const CACHE_VERSION = '1761566343|7188182275';
 /** @type {string} */
 const CACHE_PREFIX = 'Quebra Cabeça-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
