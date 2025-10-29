4daa2ca371a011c90b0c947907f61f45e613deae
Diogo RBG <diogorbg@gmail.com>
2025-10-29 20:43:54 -0300

    update shader

--- godotQuebraCabeca/quebraCabeca.service.worker.js
+++ godotQuebraCabeca/quebraCabeca.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761566343|7188182275';
+const CACHE_VERSION = '1761781386|905079470';
 /** @type {string} */
 const CACHE_PREFIX = 'Quebra Cabeça-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
