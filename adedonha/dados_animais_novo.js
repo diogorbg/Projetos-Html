0d1557d36e9471cb0b39a97cc5372103c389aefa
Diogo RBG <diogorbg@gmail.com>
2026-06-03 19:49:47 -0300

    Update dados_animais_novo.js
    precisa revisar estas novas perguntas

--- adedonha/dados_animais_novo.js
+++ adedonha/dados_animais_novo.js
@@ -1,2 +1,173 @@
 const DADOS_ANIMAIS_NOVO = [
+    {
+        "id": "animais_ja",
+        "texto": "Nome de animais que começam com 'JA'",
+        "tags": ["Animais", "Letra J"],
+        "facil": ["Jacaré", "Jabuti", "Jatobá (inseto/percevejo)", "Jaguar"],
+        "medio": ["Jali", "Jacana", "Jacuaçu", "Jaguatirica"],
+        "dificil": ["Jaburu", "Jacupemba", "Jatuná", "Jatereba"],
+        "invalido": ["Jacaré-de-bota", "Jatobá (árvore)"]
+    },
+    {
+        "id": "animais_je",
+        "texto": "Nome de animais que começam com 'JE'",
+        "tags": ["Animais", "Letra J"],
+        "facil": ["Jerboa (rato do deserto)"],
+        "medio": ["Jereba (urubu)"],
+        "dificil": ["Jequitiranaboia", "Jegue", "Jeju (peixe)"],
+        "invalido": ["Jeová"]
+    },
+    {
+        "id": "animais_ji",
+        "texto": "Nome de animais que começam com 'JI'",
+        "tags": ["Animais", "Letra J"],
+        "facil": ["Jiboia"],
+        "medio": ["Jiquiri (rã)"],
+        "dificil": ["Jiripoca (peixe)", "Jia", "Jitirana"],
+        "invalido": ["Jipe"]
+    },
+    {
+        "id": "animais_jo",
+        "texto": "Nome de animais que começam com 'JO'",
+        "tags": ["Animais", "Letra J"],
+        "facil": ["Joaninha", "João-de-barro"],
+        "medio": ["João-bobo (ave)", "Joana-gongá (peixe)"],
+        "dificil": ["Joromelo", "Joia (besouro)"],
+        "invalido": ["Jogador"]
+    },
+    {
+        "id": "animais_ju",
+        "texto": "Nome de animais que começam com 'JU'",
+        "tags": ["Animais", "Letra J"],
+        "facil": ["Jumento"],
+        "medio": ["Jundiá (peixe)", "Jupará"],
+        "dificil": ["Juruviara (ave)", "Jupati", "Jatuti"],
+        "invalido": ["Juiz", "Jumentada"]
+    },
+    {
+        "id": "animais_la",
+        "texto": "Nome de animais que começam com 'LA'",
+        "tags": ["Animais", "Letra L"],
+        "facil": ["Lagartixa", "Lagarto", "Lagosta", "Lagostim", "Larva", "Lagarta"],
+        "medio": ["Lamantim", "Lavadeira (ave/inseto)"],
+        "dificil": ["Laimargos (tubarão)", "Lambreta (molusco)"],
+        "invalido": ["Lactante", "Larvário"]
+    },
+    {
+        "id": "animais_le",
+        "texto": "Nome de animais que começam com 'LE'",
+        "tags": ["Animais", "Letra L"],
+        "facil": ["Leão", "Lesma", "Lama", "Lépis", "Lêmure", "Leopardo", "Leva"],
+        "medio": ["Leão-marinho", "Leva-mar (ave)"],
+        "dificil": ["Lera", "Lepadomorfo", "Lestodonte (extinto)"],
+        "invalido": ["Leoa", "Lebrão"]
+    },
+    {
+        "id": "animais_li",
+        "texto": "Nome de animais que começam com 'LI'",
+        "tags": ["Animais", "Letra L"],
+        "facil": ["Lince"],
+        "medio": ["Linguado (peixe)", "Limpa-vidro (peixe)"],
+        "dificil": ["Limulo", "Lira (ave)", "Lírio-do-mar"],
+        "invalido": ["Linhagem"]
+    },
+    {
+        "id": "animais_lo",
+        "texto": "Nome de animais que começam com 'LO'",
+        "tags": ["Animais", "Letra L"],
+        "facil": ["Lobo", "Lontra", "Louva-a-deus"],
+        "medio": ["Lobo-marinho", "Lobo-guará"],
+        "dificil": ["Lóris", "Lona (molusco)"],
+        "invalido": ["Lobinho", "Lobisomem"]
+    },
+    {
+        "id": "animais_lu",
+        "texto": "Nome de animais que começam com 'LU'",
+        "tags": ["Animais", "Letra L"],
+        "facil": ["Lula", "Lula-gigante"],
+        "medio": ["Lúcio (peixe)", "Lulu (ave)"],
+        "dificil": ["Lula-vampira", "Lugarito"],
+        "invalido": ["Luzerna"]
+    },
+    {
+        "id": "animais_ma",
+        "texto": "Nome de animais que começam com 'MA'",
+        "tags": ["Animais", "Letra M"],
+        "facil": ["Macaco", "Mariposa", "Marreco", "Marimbondo", "Mamute", "Marta", "Mariquita"],
+        "medio": ["Marabu", "Macaquinho-do-babaçu", "Manta (arraia)", "Mandril"],
+        "dificil": ["Mambira", "Macuru", "Macaá", "Mapará (peixe)"],
+        "invalido": ["Mamífero", "Macho"]
+    },
+    {
+        "id": "animais_me",
+        "texto": "Nome de animais que começam com 'ME'",
+        "tags": ["Animais", "Letra M"],
+        "facil": ["Melga (inseto)", "Melro"],
+        "medio": ["Merluza", "Merergon"],
+        "dificil": ["Mergulhão (ave)", "Meirinho", "Melopsitaco"],
+        "invalido": ["Medusa (fase de vida de cnidários, embora aceito como água-viva)"]
+    },
+    {
+        "id": "animais_mi",
+        "texto": "Nome de animais que começam com 'MI'",
+        "tags": ["Animais", "Letra M"],
+        "facil": ["Minhoca", "Mico", "Mico-leão-dourado"],
+        "medio": ["Milhafre (ave)", "Milpés"],
+        "dificil": ["Miru", "Mitu (ave)", "Mictis"],
+        "invalido": ["Microbiota", "Microscópico"]
+    },
+    {
+        "id": "animais_mo",
+        "texto": "Nome de animais que começam com 'MO'",
+        "tags": ["Animais", "Letra M"],
+        "facil": ["Morcego", "Mosca", "Mosquito", "Morsa", "Mocho (coruja)"],
+        "medio": ["Molusco", "Mona (macaco)"],
+        "dificil": ["Moreia", "Mocó (roedor)", "Mofumbo"],
+        "invalido": ["Molusco (classe/filo)"]
+    },
+    {
+        "id": "animais_mu",
+        "texto": "Nome de animais que começam com 'MU'",
+        "tags": ["Animais", "Letra M"],
+        "facil": ["Mula", "Muçuã (cágado)", "Musaranho"],
+        "medio": ["Mussurana (cobra)", "Muçum (peixe)"],
+        "dificil": ["Muntíaco", "Muaná", "Muriqui"],
+        "invalido": ["Mutação", "Multicelular"]
+    },
+    {
+        "id": "animais_na",
+        "texto": "Nome de animais que começam com 'NA'",
+        "tags": ["Animais", "Letra N"],
+        "facil": ["Naja", "Namorado (peixe)"],
+        "medio": ["Nandubai", "Nambú"],
+        "dificil": ["Narval", "Nacua", "Niala (antílope)"],
+        "invalido": ["Nativo", "Nascituro"]
+    },
+    {
+        "id": "animais_ne",
+        "texto": "Nome de animais que começam com 'NE'",
+        "tags": ["Animais", "Letra N"],
+        "facil": ["Neon (peixe)"],
+        "medio": ["Negrinho (ave)", "Neinei (ave)"],
+        "dificil": ["Nécton", "Nenharé", "Necturo"],
+        "invalido": ["Néon (elemento químico)"]
+    },
+    {
+        "id": "animais_ni",
+        "texto": "Nome de animais que começam com 'NI'",
+        "tags": ["Animais", "Letra N"],
+        "facil": ["Niquim (peixe venenoso)"],
+        "medio": ["Niala"],
+        "dificil": ["Nhandu", "Nico (macaco)", "Nightjar (noitibó em inglês)"],
+        "invalido": ["Ninho"]
+    },
+    {
+        "id": "animais_no",
+        "texto": "Nome de animais que começam com 'NO'",
+        "tags": ["Animais", "Letra N"],
+        "facil": ["Noitibó (ave)"],
+        "medio": ["Notonecta (inseto aquático)"],
+        "dificil": ["Nodí", "Nortista (ave)", "Notomys"],
+        "invalido": ["Nómade", "Noturno"]
+    },
 ]
