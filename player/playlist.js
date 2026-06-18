ac849a3ce6ffb23f1d89cc87a9ce2048ae45227c
Diogo RBG <diogorbg@gmail.com>
2026-06-18 19:09:31 -0300

    Update playlist.js

--- player/playlist.js
+++ player/playlist.js
@@ -1,13 +1,32 @@
 const playlist = [
     {
+        titulo: "Tribal Swing - Dandelion Moon Carnival",
+        audioUrl: "file:///C:/Users/diogo/Music/%E3%80%90Tribal%20Swing%E3%80%91Dandelion%20Moon%20Carnival%EF%BD%9CGraceful%20Electro%20Swing%20-%20SWING%20SWING%20SWING%20(youtube).mp3",
+        capaUrl: "file:///C:/Users/diogo/Music/%E3%80%90Tribal%20Swing%E3%80%91Dandelion%20Moon%20Carnival%EF%BD%9CGraceful%20Electro%20Swing%20-%20SWING%20SWING%20SWING%20(youtube).jpg?w=1280&h=720&fit=crop",
+    },
+    {
+        titulo: "Coffee Time ☕ Lo-fi Jazz HipHop",
+        audioUrl: "file:///C:/Users/diogo/Music/Coffee%20Time%20%E2%98%95%20Lo-fi%20Jazz%20HipHop%20Do%20you%20like%20your%20coffee%20sweetened%20-%20Chill%20Lofi%20Capybara%20(youtube).mp3",
+        capaUrl: "file:///C:/Users/diogo/Music/Coffee%20Time%20%E2%98%95%20Lo-fi%20Jazz%20HipHop%20Do%20you%20like%20your%20coffee%20sweetened%20-%20Chill%20Lofi%20Capybara%20(youtube).jpg"
+    },
+    {
         titulo: "Donkey Kong Country • Full Lofi Jazz Remix Album",
-        // artista: "Artista Alpha",
         audioUrl: "file:///C:/Users/diogo/Music/Donkey%20Kong%20Country%20%E2%80%A2%20Full%20Lofi%20Jazz%20Remix%20Album%20(Cozy%2016-Bit%20Jungle%20Beats)%20-%20Pixel%20Save%20Room%20(youtube).mp3",
         capaUrl: "file:///C:/Users/diogo/Music/Donkey%20Kong%20Country%20%E2%80%A2%20Full%20Lofi%20Jazz%20Remix%20Album%20(Cozy%2016-Bit%20Jungle%20Beats)%20-%20Pixel%20Save%20Room%20(youtube).jpg?w=1280&h=720&fit=crop"
     },
     {
-        titulo: "Música Exemplo 2",
-        audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-        capaUrl: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=1280&h=720&fit=crop"
+        titulo: "Super Mario Bros - Super Swing Brothers 🎷💃",
+        audioUrl: "file:///C:/Users/diogo/Music/Super%20Mario%20Bros%20%E2%80%93%20Super%20Swing%20Brothers%20%F0%9F%8E%B7%F0%9F%92%83%20Electro%20Swing%20Remix%20-%20PixelBeats%20(youtube).mp3",
+        capaUrl: "file:///C:/Users/diogo/Music/Super%20Mario%20Bros%20%E2%80%93%20Super%20Swing%20Brothers%20%F0%9F%8E%B7%F0%9F%92%83%20Electro%20Swing%20Remix%20-%20PixelBeats%20(youtube).jpg"
+    },
+    {
+        titulo: "Yoshi's Music Lo-Fi 🎧 Chill Beats to Study & Relax",
+        audioUrl: "file:///C:/Users/diogo/Music/Yoshi's%20Music%20Lo-Fi%20%F0%9F%8E%A7%20Chill%20Beats%20to%20Study%20&%20Relax%20Lo-Fi%20Mornings%20%E2%98%95%EF%B8%8F%E2%9C%A8%20-%20NESTALGIA%20(youtube).mp3",
+        capaUrl: "file:///C:/Users/diogo/Music/Yoshi's%20Music%20Lo-Fi%20%F0%9F%8E%A7%20Chill%20Beats%20to%20Study%20&%20Relax%20Lo-Fi%20Mornings%20%E2%98%95%EF%B8%8F%E2%9C%A8%20-%20NESTALGIA%20(youtube).jpg"
+    },
+    {
+        titulo: "고양이와 Iwamizu의 Lofi Jazz Piano",
+        audioUrl: "file:///C:/Users/diogo/Music/%EA%B3%A0%EC%96%91%EC%9D%B4%EC%99%80%20Iwamizu%EC%9D%98%20Lofi%20Jazz%20Piano%20%E2%80%A2%20%EA%B3%B5%EB%B6%80%ED%95%A0%EB%95%8C,%20%EC%A7%91%EC%A4%91%ED%95%A0%EB%95%8C,%20%EC%BD%94%EB%94%A9%ED%95%A0%EB%95%8C%20%E2%80%A2%203%20hours%20-%20yellow%20mixtape%20(youtube).mp3",
+        capaUrl: "file:///C:/Users/diogo/Music/%EA%B3%A0%EC%96%91%EC%9D%B4%EC%99%80%20Iwamizu%EC%9D%98%20Lofi%20Jazz%20Piano%20%E2%80%A2%20%EA%B3%B5%EB%B6%80%ED%95%A0%EB%95%8C,%20%EC%A7%91%EC%A4%91%ED%95%A0%EB%95%8C,%20%EC%BD%94%EB%94%A9%ED%95%A0%EB%95%8C%20%E2%80%A2%203%20hours%20-%20yellow%20mixtape%20(youtube).jpg"
     },
 ]
