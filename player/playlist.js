fa11f92aea2c577893270dd2d945aef8cfcd8780
Diogo RBG <diogorbg@gmail.com>
2026-06-21 14:30:38 -0300

    reorganizando

--- player/playlist.js
+++
@@ -1,5 +0,0 @@
-const playlist = [
-    { titulo: "Música 1", artista: "Artista Alpha", audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", capaUrl: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=1280&h=720&fit=crop" },
-    { titulo: "Música 2", artista: "Artista Beta", audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3", capaUrl: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=1280&h=720&fit=crop" },
-    { titulo: "Música 3", artista: "Artista Gamma", audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3", capaUrl: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=1280&h=720&fit=crop" }
-]
